d459a5fcc4bf5effea86cebe3d529e1f0c59afeb
whatwg6 <whatwg6@users.noreply.github.com>
2026-06-19 01:55:19 +0800

    ci: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/worldclup26/' : '/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
